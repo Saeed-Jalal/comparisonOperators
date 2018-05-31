e97f61dc723fe2ecd5d0616bab189990fe9f0ec8
Saeed-Jalal <syedsaeedjalal@gmail.com>
2018-05-31 14:48:36 +0300

    How comparison operator work in Java

---
+++ src/comparisonOperators.java
@@ -0,0 +1,53 @@
+public class comparisonOperators {
+    //comparison operators are also called Relational Operators
+    public static void main(String args[]){
+        int num1 = 50;
+        int num2 = 10;
+        //num1 == num2
+
+        if(num1 == num2){
+            System.out.println("This is TalkMe day 1.");
+
+        }
+        else{
+            System.out.println("This is TalkMe day 2.");
+        }
+        //num1!= num2
+        if (num1 != num2) {
+            System.out.println("This is TalkMe day 3.");
+        }
+        else {
+            System.out.println("This is TalkMe day 5");
+
+        }
+        //num1 > num2
+        if(num1 > num2){
+            System.out.println("This is TalkMe day 6.");
+        }
+        else{
+            System.out.println("This is TalkMe day 7.");
+        }
+        //num1 >= num2
+        if(num1 >= num2){
+            System.out.println("This is TalkMe day 8.");
+        }
+        else{
+            System.out.println("This is TalkMe day 9.");
+        }
+        //num1 < num2
+        if(num1 < num2){
+            System.out.println("This is TalkMe day 10.");
+        }
+        else{
+            System.out.println("This is TalkMe day 11.");
+        }
+        //num1 <= num2
+        if(num1 <= num2){
+            System.out.println("This is TalkMe day 12.");
+        }
+        else{
+            System.out.println("This is TalkMe day 13.");
+        }
+
+    }
+}
